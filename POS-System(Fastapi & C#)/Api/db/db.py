8b886e813335eaef242b3eb9b7e2fa94f5f2bd51
taidavis1 <73322981+taidavis1@users.noreply.github.com>
2025-03-21 04:39:40 -0700

    Update db.py

--- POS-System(Fastapi & C#)/Api/db/db.py
+++ POS-System(Fastapi & C#)/Api/db/db.py
@@ -2,9 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker , Session
 
-SQLALCHEMY_DATABASE_URI =  "mysql+pymysql://selena:123giadinh@74.208.246.165:3306/admin_pasadena"
-# mysql+pymysql://selena:123giadinh@74.208.246.165:3306/admin_pasadena"
-# mysql+pymysql://root:123giadinh@localhost/testdb
+SQLALCHEMY_DATABASE_URI =  ""
 engine = create_engine(
     SQLALCHEMY_DATABASE_URI
 )
